6aaf8af983e406b38a13c00e9549b1560315f1c0
Sergej Birklin <sergej.birklin@skillworks.de>
2015-11-03 20:37:15 +0100

    Added idea to gitignore and changed Controller for PlayFramework

--- de.htwg.se.2powerN/src/de/htwg/se/tpn/TwoPN.java
+++ de.htwg.se.2powerN/src/de/htwg/se/tpn/TwoPN.java
@@ -4,25 +4,38 @@ import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
+import de.htwg.se.tpn.controller.TpnController;
 import de.htwg.se.tpn.controller.TpnControllerFactory;
 import de.htwg.se.tpn.controller.TpnControllerInterface;
 import de.htwg.se.tpn.view.GUI;
 import de.htwg.se.tpn.view.TUI;
 
 public final class TwoPN {
-	@Inject private TpnControllerFactory controllerfactory;
+	@Inject
+	private TpnControllerFactory controllerfactory;
 	private static final int FIELDSIZE = 4;
+	private TpnControllerInterface controller;
+	private static TwoPN instance = null;
 
-	public void run() {
-		TpnControllerInterface controller = controllerfactory.create(FIELDSIZE, 1);
-
+	public TwoPN() {
+		controller = new TpnController(FIELDSIZE, 1);
 		new GUI(controller);
 		new TUI(controller);
 	}
-	
+
+	public static TwoPN getInstance() {
+		if (instance == null) {
+			Injector injector = Guice.createInjector(new TpNModule());
+			instance = injector.getInstance(TwoPN.class);
+		}
+		return instance;
+	}
+
+	public TpnControllerInterface getController() {
+		return this.controller;
+	}
+
 	public static void main(String[] args) {
-		Injector injector = Guice.createInjector(new TpNModule());
-        TwoPN main = injector.getInstance(TwoPN.class);
-        main.run();
+		TwoPN game = getInstance();
 	}
 }
