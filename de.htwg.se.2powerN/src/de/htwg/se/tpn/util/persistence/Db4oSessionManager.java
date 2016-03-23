7d2260d2359099faebce7760e302c8febd72cfbc
Sergej Birklin <sergej.birklin@namics.com>
2016-03-23 11:18:03 +0100

    implemented DB4oSessionManager class
    - createDB
    - getDbObjectContainer

--- de.htwg.se.2powerN/src/de/htwg/se/tpn/util/persistence/Db4oSessionManager.java
+++ de.htwg.se.2powerN/src/de/htwg/se/tpn/util/persistence/Db4oSessionManager.java
@@ -1,7 +1,37 @@
 package de.htwg.se.tpn.util.persistence;
 
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.config.EmbeddedConfiguration;
+
 /**
  * Created by Sergej on 23/03/16.
  */
 public class Db4oSessionManager {
+    private static final String FILENAME = "tpn.db";
+
+    private static ObjectContainer db;
+
+    private Db4oSessionManager() {
+    }
+
+    /**
+     * @return objectContainer
+     */
+    public static ObjectContainer getDbObjectContainer() {
+        if (db == null) {
+            createDB();
+        } else if (db.ext().isClosed()) {
+            createDB();
+        }
+        return db;
+    }
+
+    /**
+     * create DB
+     */
+    private static void createDB() {
+        EmbeddedConfiguration configuration = Db4oEmbedded.newConfiguration();
+        db = Db4oEmbedded.openFile(configuration, FILENAME);
+    }
 }
